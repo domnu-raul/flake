83d12c3ef144ed0cfcd41fec01ad0b69343b72ae
domnu-raul <sir.c4ppuccin0@gmail.com>
2024-07-17 10:36:18 +0300

    disko

---
+++ disko-config.nix
@@ -0,0 +1,40 @@
+{
+  disko.devices = {
+    disk = {
+      my-disk = {
+        device = "/dev/sda";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              type = "EF00";
+              size = "500M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            {
+              name = "swap";
+              size = "4G";
+              content = {
+                type = "swap";
+                resumeDevice = true;
+              };
+            }
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
