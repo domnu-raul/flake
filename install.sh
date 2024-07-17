a24c22d1d7dbf1f37237ac3303b8ece929b4f657
domnu-raul <sir.c4ppuccin0@gmail.com>
2024-07-17 14:35:39 +0300

    updated the install script

--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
-# disko partitions
+#PARTITIONING
 nix --experimental-features 'nix-command flakes' run github:nix-community/disko -- --mode disko ./disko-config.nix
+
+#HARDWARE CONFIGURATION
 nix-generate-config --root /mnt
 cp /mnt/etc/nixos/hardware-configuration.nix /mnt/etc/nixos/hardware-configuration.nix
+
+#INSTALLATION
 nixos-install --flake .#nixos
+
+#COPY CONFIGURATION
+mkdir /mnt/home/domnuraul/nix 
+cp ./* /mnt/home/domnuraul/nix
